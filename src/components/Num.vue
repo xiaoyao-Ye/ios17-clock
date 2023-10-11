4f892d44f765caf3107812d42cf57b20b133e0e1
Ghosteye <yqcyecong@163.com>
2023-10-11 15:42:38 +0800

    fix: num error when find-out animate

--- src/components/Num.vue
+++ src/components/Num.vue
@@ -3,37 +3,53 @@ const props = defineProps<{
   num: number;
   delay: number;
 }>();
-const nums = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const nums = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const rotate = ref(getRandom(-15, 15));
 const rotate_old = ref();
-const isFirst = ref(true);
+const key = ref(new Date().getTime());
+const num_current = ref(props.num);
+const num_old = ref();
 function getRandom(min: number, max: number) {
   return Math.round(Math.random() * (max - min) + min);
 }
 watch(
   () => props.num,
-  () => {
-    const random = getRandom(-15, 15);
-    if (isFirst.value && rotate_old.value) isFirst.value = false;
+  newValue => {
+    console.log("num", props.num);
+    num_old.value = num_current.value;
+    num_current.value = newValue;
     rotate_old.value = rotate.value;
-    rotate.value = random;
+    key.value = new Date().getTime();
+    // const random = getRandom(-15, 15);
+    // if (isFirst.value && !oldNum.value) isFirst.value = false;
+    // rotate_old.value = rotate.value;
+    // rotate.value = random;
+    // if (!isFirst.value) oldNum.value = currentNum.value;
+    // currentNum.value = props.num;
   },
 );
 </script>
 
 <template>
   <div>
-    <div v-for="value in nums" :key="value">
-      <div v-show="!isFirst && num === value + 1" class="find-out" :style="`animation-delay: 0.${delay}s;`">
-        <div :style="`transform: rotate(${rotate_old}deg)`">
-          {{ value === -1 ? "9" : value }}
-        </div>
-      </div>
-      <div v-show="num === value" class="find-in" :style="`animation-delay: 0.${4 + delay}s;`">
-        <div :style="`transform: rotate(${rotate}deg)`">
-          {{ value }}
-        </div>
-      </div>
+    <!-- <div v-for="value in nums" :key="value"> -->
+    <!-- <div v-show="!isFirst && num === value + 1" class="find-out" :style="`animation-delay: 0.${delay}s;`"> -->
+    <!-- <div :style="`transform: rotate(${rotate_old}deg)`"> -->
+    <!-- 这里value===-1时, 秒是9十秒是5?,分是9十分是5?,时是2,小时是4  所以还是要记录old和new -->
+    <!-- {{ value === -1 ? "9" : value }} -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- <div v-show="num === value" class="find-in" :style="`animation-delay: 0.${4 + delay}s;`"> -->
+    <!-- <div :style="`transform: rotate(${rotate}deg)`"> -->
+    <!-- {{ value }} -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <div v-show="num_old === undefined" :key="key" class="find-out" :style="`animation-delay: 0.${delay}s;`">
+      <div :style="`transform: rotate(${rotate_old}deg)`">{{ num_old }}</div>
+    </div>
+    <div v-show="num_current === undefined" :key="key" class="find-in" :style="`animation-delay: 0.${4 + delay}s;`">
+      <div :style="`transform: rotate(${rotate}deg)`">{{ num_current }}</div>
     </div>
   </div>
 </template>
