e329dd3fc4863b1ca635df2acd97f3f378947538
_Ghosteye <yqcyecong@163.com>
2024-03-21 16:47:28 +0800

    feat: Base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "vite";
-import path from "path";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 import UnoCSS from "unocss/vite";
-import Components from "unplugin-vue-components/vite";
 import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ios17-clock/",
   resolve: {
     alias: {
       "~/": `${path.resolve(__dirname, "src")}/`,
